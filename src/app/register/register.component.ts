ffaaaf6bb0de64eb8178fb989e8b31da54e8c8da
AmiraAbdelhalim <amira.a.a.hashim@gmail.com>
2021-10-11 16:25:41 +0200

    register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IMenuItem, IMenuTrigger } from '@covalent/core/dynamic-menu';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // #region declare variables
 
-  ngOnInit(): void {
+  listOfLang: IMenuItem[];
+
+  // #endregion
+
+  // #region constructor
+
+  constructor(
+    private router: Router,
+    public translate: TranslateService,
+  ) {
+    // init variables
+    this.listOfLang = [
+      { action: 'ar', text: 'العربية' },
+      { action: 'en', text: 'English' }
+    ];
   }
+
+  // #endregion
+
+  // #region ngOnInit
+
+  ngOnInit(): void { }
+
+  // #endregion
+
+  // #region main actions
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
+
+  setTriggerText(title: string, icon?: string): IMenuTrigger {
+    return { text: title, icon };
+  }
+
+  registeredToLogin() {
+    this.router.navigate(['/'])
+  }
+
+  // #endregion
 
 }
