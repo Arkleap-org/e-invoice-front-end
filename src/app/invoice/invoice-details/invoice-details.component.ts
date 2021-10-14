eca762b121e800bafd2237a2a10cf7f69a6137a5
AmiraAbdelhalim <amira.a.a.hashim@gmail.com>
2021-10-14 13:31:54 +0200

    receiver info form

--- src/app/invoice/invoice-details/invoice-details.component.ts
+++ src/app/invoice/invoice-details/invoice-details.component.ts
@@ -1,3 +1,4 @@
+// angular core
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,70 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './invoice-details.component.html',
   styleUrls: ['./invoice-details.component.scss']
 })
+
 export class InvoiceDetailsComponent implements OnInit {
 
-  constructor() { }
+  // #region declare variables
+
+  isNewReceiver: boolean;
+  listOfReceivers: {}[];
+  listOfReceiverType: { label: string, value: string }[];
+  listOfCountries: { code: string, en_name: string, ar_name: string }[];
+
+
+  // #endregion
+
+  // #region constructor
+
+  constructor() {
+    // init variables
+    this.isNewReceiver = false;
+    this.listOfReceivers = [
+      {
+        id: 1,
+        name: "khalaf"
+      }
+    ];
+    this.listOfReceiverType = [
+      {
+        label: 'Business',
+        value: 'B'
+      },
+      {
+        label: 'Natural Preson',
+        value: 'NP'
+      },
+      {
+        label: 'Foreigner',
+        value: 'F'
+      }
+    ];
+
+    this.listOfCountries = [
+      {
+        code: 'EG',
+        en_name: 'Egypt',
+        ar_name: 'مصر'
+      },
+      {
+        code: 'UK',
+        en_name: 'United Kingdom',
+        ar_name: 'المملكة المتحدة'
+      }
+    ];
+  }
+
+  // #endregion
+
+  // #region ngOnInit
 
   ngOnInit(): void {
   }
+
+  // #endregion
+
+  // #region main actions
+
+  // #end region
 
 }
