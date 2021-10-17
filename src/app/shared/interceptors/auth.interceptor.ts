fe64dc91fa6397e256150063f9a106d0ac0dedc8
walaa <walaa9402@gmail.com>
2021-10-17 14:02:25 +0200

    set header

--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -1,25 +1,27 @@
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { SecurityService } from "../services/security.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() { }
+  constructor(
+    private securityService: SecurityService
+  ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // add authorization header with jwt token
-    if (!request.url.startsWith('http') || !request.url.startsWith('https')) {
-      // const authReq = request.clone({
-      //     headers: request.headers
-      //         .set("Authorization", `Bearer }`)
-      //         .set("Content-Type", "application/json")
-      //         .set("Cache-Control", "no-cache")
-      //         .set("Pragma", "no-cache")
-      // });
+    if (!request.url.startsWith('http') && !request.url.startsWith('https') && !request.url.includes('assets')) {
+      const authReq = request.clone({
+        headers: request.headers
+          .set("Authorization", `Bearer ${this.securityService.jwtToken}`)
+          .set("Content-Type", "application/json")
+          .set("Cache-Control", "no-cache")
+          .set("Pragma", "no-cache")
+      });
       // pass on the cloned request instead of the original request.
-      // return next.handle(authReq);
-      return next.handle(request);
+      return next.handle(authReq);
     } else {
       return next.handle(request);
     }
