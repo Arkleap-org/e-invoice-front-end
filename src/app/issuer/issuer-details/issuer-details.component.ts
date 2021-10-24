cbce0503d1755faf651c72656c712ccca782dc17
AmiraAbdelhalim <amira.a.a.hashim@gmail.com>
2021-10-24 11:35:48 +0200

    edit country to display ar and en in issuer

--- src/app/issuer/issuer-details/issuer-details.component.ts
+++ src/app/issuer/issuer-details/issuer-details.component.ts
@@ -14,6 +14,7 @@ import { IssuerAddressDto, IssuerDto } from "../../shared/models/issuer.model";
 import { IssuerService } from "../../shared/services/issuer.service";
 import { ListsService } from "../../shared/services/lists.service";
 import { DialogService } from "../../shared/services/dialog.service";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   templateUrl: "./issuer-details.component.html",
@@ -47,7 +48,8 @@ export class IssuerDetailsComponent implements OnInit {
     private dialogService: DialogService,
     private issuerService: IssuerService,
     private listsService: ListsService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    public translate: TranslateService
   ) {
 
     // init variables
