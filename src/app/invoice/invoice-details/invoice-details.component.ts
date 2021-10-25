253e0e5ed665f6ba6c78b5d412388bf3d4978572
AmiraAbdelhalim <amira.a.a.hashim@gmail.com>
2021-10-25 15:50:01 +0200

    create forms invoice

--- src/app/invoice/invoice-details/invoice-details.component.ts
+++ src/app/invoice/invoice-details/invoice-details.component.ts
@@ -1,5 +1,6 @@
 // angular core
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 
 // modals
@@ -23,11 +24,15 @@ export class InvoiceDetailsComponent implements OnInit {
 
   // #region declare variables
 
-  isNewReceiver: boolean;
+  // names of lists
   listOfReceivers: ReceiverDto[];
   listOfDocumentTypes: { label: string, value: string }[];
   listOfDocumentTypeVersions: string[];
   listOfItems: { id: number, name: string }[];
+
+  // names of forms
+  invoiceForm!: FormGroup
+  lines!: FormArray;
 
 
   // #endregion
@@ -38,26 +43,17 @@ export class InvoiceDetailsComponent implements OnInit {
     private dialogService: DialogService,
     private invoiceService: InvoiceService,
     private receiverService: ReceiverService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private formBuilder: FormBuilder
   ) {
     // init variables
-    this.isNewReceiver = false;
 
     this.listOfReceivers = [];
 
     this.listOfDocumentTypes = [
-      {
-        label: "Invoice",
-        value: "I"
-      },
-      {
-        label: "Credit Memo",
-        value: "C"
-      },
-      {
-        label: "Debit Memo",
-        value: "D"
-      }
+      { label: "Invoice", value: "I" },
+      { label: "Credit Memo", value: "C" },
+      { label: "Debit Memo", value: "D" }
     ];
 
     this.listOfDocumentTypeVersions = [
@@ -82,6 +78,45 @@ export class InvoiceDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.listReceivers();
+  }
+
+  // #endregion
+
+  // #region init forms
+
+  initForms() {
+
+  }
+
+  initInvoiceForm() {
+    this.invoiceForm = this.formBuilder.group({
+      document_type: ['', Validators.required],
+      document_type_version: ['', Validators.required],
+      internal_id: ['', Validators.required],
+      lines: this.formBuilder.array([this.createLines()])
+    });
+  }
+
+  createLines(): FormGroup {
+    return this.formBuilder.group({
+      item: [''],
+      description: [''],
+      quantity: [''],
+      unit_price: [''],
+      sales_total: [''],
+      discount_amount: [''],
+      tax_amount: [''],
+      net_total: ['']
+    });
+  }
+
+  addAccount(): void {
+    this.lines = this.invoiceForm.get('lines') as FormArray;
+    this.lines.push(this.createLines())
+  }
+
+  deleteRow(index: number) {
+    this.lines.removeAt(index);
   }
 
   // #endregion
