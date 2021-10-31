c8b7a4f921a3c46eef97b857c02cb01b3ad1023f
yisaleh <yara_ahmed_saleh@yahoo.com>
2021-10-31 23:18:40 +0200

    change view user to form

--- src/app/admin/user-view/user-view.component.ts
+++ src/app/admin/user-view/user-view.component.ts
@@ -1,5 +1,7 @@
 // angular modules
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 // models
@@ -20,6 +22,9 @@ export class UserViewComponent implements OnInit {
 
   userId!: number;
   userDetails!: UserRequestDto;
+  userForm!: FormGroup;
+  isSubmitted: boolean;
+
 
   // #endregion
 
@@ -27,10 +32,14 @@ export class UserViewComponent implements OnInit {
 
   constructor(
     private userService: UserService,
+    private formBuilder: FormBuilder,
+    
     private route: ActivatedRoute
   ) { 
 
-    this.userDetails = new UserRequestDto
+    this.userDetails = new UserRequestDto;
+    this.isSubmitted = false;
+
   }
 
   // #endregion
@@ -40,9 +49,32 @@ export class UserViewComponent implements OnInit {
   ngOnInit(): void {
     this.userId = this.route.snapshot.params["id"]
     this.getUserById();
+    this.initForm();
   }
   // #endregion
+  // #region init forms
 
+  initForm() {
+    this.userForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      is_staff: ['', Validators.required],
+      is_superuser: ['', Validators.required],
+      is_active: ['', Validators.required],
+      date_joined: ['', Validators.required],
+      last_login: ['', Validators.required],
+      issuer: ['', Validators.required],
+    });
+  }
+
+  // form controls
+  get itemsFormControls() {
+    return this.userForm.controls;
+  }
+
+  // #endregion
   // #region main actions
 
   getUserById(){
