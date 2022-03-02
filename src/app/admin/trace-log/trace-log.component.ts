d27b132aa41d45aed8b2b185ea47052007197ee2
shoroukshaheen44 <shorouk.shaheen@arkleap.com>
2022-03-02 10:42:02 +0200

    trace log

---
+++ src/app/admin/trace-log/trace-log.component.ts
@@ -0,0 +1,124 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ResponseDto } from 'src/app/shared/models/api-response.model';
+import { DialogService } from 'src/app/shared/services/dialog.service';
+import { UserService } from 'src/app/shared/services/user.service';
+
+@Component({
+  selector: 'app-trace-log',
+  templateUrl: './trace-log.component.html',
+  styleUrls: ['./trace-log.component.scss']
+})
+export class TraceLogComponent implements OnInit {
+
+ // #region declare variables
+
+ @ViewChild(MatSort) sort!: MatSort;
+ @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+ traceLogDataSource: MatTableDataSource<any>;
+ displayedColumns: string[];
+
+ pageSize = 5;
+ currentPage = 0;
+ pageSizeOptions: number[] = [5, 10, 25, 100];
+
+ // #endregion
+
+ // #region constructor
+
+ constructor(
+   private userService: UserService,
+   private dialogService: DialogService
+ ) {
+
+   // init variables
+   this.traceLogDataSource = new MatTableDataSource();
+   this.displayedColumns = ['entity', 'trace_msg', 'data', 'url', 'date_time', 'os_user', 'user', ];
+ }
+
+ // #endregion
+
+ // #region ngOnInit
+
+ ngOnInit(): void {
+   this.loadControls();
+ }
+
+ // #endregion
+
+ // #region load controls
+
+ loadControls() {
+   this.listLogTracres();
+ }
+
+ listLogTracres() {
+   this.userService.listTraceLog(this.currentPage + 1, this.pageSize).subscribe((response: ResponseDto) =>{
+     this.traceLogDataSource.data = response.data;
+     setTimeout(() => {
+      this.paginator.pageIndex = this.currentPage;
+      this.paginator.length = response.count;            
+    });
+  }
+  )
+ }
+
+ // #endregion
+
+ // #region ngAfterViewInit
+
+ ngAfterViewInit() {
+   this.traceLogDataSource.paginator = this.paginator;
+   this.traceLogDataSource.sort = this.sort;
+ }
+
+ // #endregion
+    pageChanged(event: PageEvent) {
+      this.pageSize = event.pageSize;
+      this.currentPage = event.pageIndex;
+      this.listLogTracres();
+    }
+ // #region main action
+
+//  deleteUser(user: UserRequestDto) {
+//    if (user.issuer) this.dialogService.alertMessege('User with issuer can\'t be deleted')
+//    else {
+//      this.dialogService.confirmDelete().then((result) => {
+//        if (result.isConfirmed) {
+//          this.userService.deleteUser(user.id).subscribe((response) => {
+//            this.dialogService.savedSuccessfully(user.username + 'deleted!')
+//            this.listUsers();
+//          });
+//        }
+//      });
+//    }
+//  }
+
+//  activateUser(user: UserRequestDto) {
+//    this.userService.activateUser(user.id).subscribe((response: ResponseDto) => {
+//      this.dialogService.savedSuccessfully(user.username + 'Activated')
+//      this.listUsers();
+//    });
+//  }
+
+//  deactivateUser(user: UserRequestDto) {
+//    this.userService.deactivateUser(user.id).subscribe((response: ResponseDto) => {
+//      this.dialogService.savedSuccessfully(user.username + 'Deactivated')
+//      this.listUsers();
+//    });
+//  }
+
+ applyFilter(event: Event) {
+   const filterValue = (event.target as HTMLInputElement).value;
+   this.traceLogDataSource.filter = filterValue.trim().toLowerCase();
+   if (this.traceLogDataSource.paginator) {
+     this.traceLogDataSource.paginator.firstPage();
+   }
+ }
+
+ // #endregion
+
+}
